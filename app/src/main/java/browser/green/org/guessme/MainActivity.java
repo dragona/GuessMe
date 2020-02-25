20bd245a538f4c42c76636c8eb84e46cadce71ef
Feilong <feilong.dragona@gmail.com>
2020-02-25 11:00:48 +0800

    fix ui and button click

--- app/src/main/java/browser/green/org/guessme/MainActivity.java
+++ app/src/main/java/browser/green/org/guessme/MainActivity.java
@@ -2,68 +2,64 @@ package browser.green.org.guessme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText;
-    TextView textView_1;
-    TextView textView_2;
-    TextView textView_3;
+    TextView display;
     TextView tips;
-    int number=(int)(Math.random()*1000);
+    private int number;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final int number=(int)(Math.random()*1000);
-        editText=(EditText)findViewById(R.id.editText3);
-        textView_1=(TextView)findViewById(R.id.textView);
-        textView_2=(TextView)findViewById(R.id.textView2);
-        textView_3=(TextView)findViewById(R.id.textView3);
-        tips=(TextView)findViewById(R.id.tips);
+//        final int number = (int) (Math.random() * 1000);
+        number = 234; //testing
 
-        Button button=(Button)findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                check(view,number);
+        editText = findViewById(R.id.et_input);
+        display = findViewById(R.id.tv_display);
+
+        tips = findViewById(R.id.tips);
+
+    }
+
+
+    // Handle the button click
+    public void btnCheck(View view) {
+
+        //todo: limit the number of input -what is len valid for int in Java?
+        //todo: what if the input is null?
+        String input = editText.getText().toString();
+        if (input.isEmpty()) {
+            Toast.makeText(this, "Invalid input", Toast.LENGTH_LONG).show();
+        } else {
+            // inputs are valid int
+            int value = Integer.parseInt(input);
+
+            if (value < 0 || value > 1000) // requirement was [1, 1000]
+                tips.setText("Please input a number between [1, 1000]!");
+            else if (value < number) {
+                display.setText("too small!");
+            } else if (value > number) {
+                display.setText("Too big");
+            } else {
+                Toast.makeText(this, "Got it!", Toast.LENGTH_LONG).show();
+                Log.e("TAG", "Got it");
+                display.setText("Congratulations!");
+                display.setBackgroundColor(Color.GREEN);
+                display.setTextColor(Color.WHITE);
+
             }
-        });
-
-    }
-
-    public void check(View view,int number){
-        int temp=Integer.parseInt(editText.getText().toString());
-        if(editText.getText().toString().equals(null))
-            tips.setText("Please input a number!");
-        else if(temp<0||temp>9999)
-            tips.setText("Please input a number between 0 and 9999!");
-        else if(temp<number){
-            textView_1.setText("too samll!");
-            textView_1.setBackgroundColor(Color.GREEN);
-            textView_3.setText("");
-            textView_3.setBackgroundColor(Color.WHITE);
-        }
-        else if(temp>number){
-            textView_3.setText("too big!");
-            textView_3.setBackgroundColor(Color.RED);
-            textView_1.setText("");
-            textView_1.setBackgroundColor(Color.WHITE);
-        }
-        else{
-            textView_2.setText("congratulation!");
-            textView_2.setBackgroundColor(Color.YELLOW);
-            textView_1.setText("");
-            textView_1.setBackgroundColor(Color.WHITE);
-            textView_3.setText("");
-            textView_3.setBackgroundColor(Color.WHITE);
         }
     }
+
+
 }
